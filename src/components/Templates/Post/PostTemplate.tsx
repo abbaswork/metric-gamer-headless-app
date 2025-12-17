bc25fdf88e5066255bebc02debec4f8830fe78e9
Abbas Khan <abbaskhanwork@gmail.com>
2025-12-17 12:38:44 +0400

    fix: expiremental blocks

--- src/components/Templates/Post/PostTemplate.tsx
+++ src/components/Templates/Post/PostTemplate.tsx
@@ -12,33 +12,33 @@ interface TemplateProps {
   node: ContentNode;
 }
 
-const componentMapper = (posts: Post, renderBlock?: string, renderComponent?: string) => {
-  //check if content blocks exist on page
-  if (posts && posts.contentBlocks) {
-    //map the blocks to components
-    return posts.contentBlocks.map((block, index) => {
-      //if renderOnly is set, ensure the block name matches
-      if (renderBlock && block?.blockName !== renderBlock) return;
+// const componentMapper = (posts: Post, renderBlock?: string, renderComponent?: string) => {
+//   //check if content blocks exist on page
+//   if (posts && posts.contentBlocks) {
+//     //map the blocks to components
+//     return posts.contentBlocks.map((block, index) => {
+//       //if renderOnly is set, ensure the block name matches
+//       if (renderBlock && block?.blockName !== renderBlock) return;
 
-      //map blocks into components
-      switch (block?.blockName) {
-        case "game-metrics-block":
-          return <ScoreCardSection key={index} blockGames={block.data.blockGames} />;
+//       //map blocks into components
+//       switch (block?.blockName) {
+//         case "game-metrics-block":
+//           return <ScoreCardSection key={index} blockGames={block.data.blockGames} />;
           
-        default:
-      }
-    });
-  }
+//         default:
+//       }
+//     });
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
 export default async function PostTemplate({ node }: TemplateProps) {
   const { post } = await fetchGraphQL<{ post: Post }>(print(PostQuery), {
     id: node.databaseId,
   });
 
-  console.log("post", post.contentBlocks);
+  // console.log("post", post.contentBlocks);
 
   return (
     <div className={styles.post}>
