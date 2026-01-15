c8fcfbcd332b40ab3024890f19821aa59e9f12b6
abbaswork <abbaskhanwork@gmail.com>
2026-01-15 16:06:59 +0400

    feat: About Section

---
+++ src/app/(subpages)/about/page.tsx
@@ -0,0 +1,9 @@
+import { AboutSection } from "@/stories/sections/About/About";
+
+export default function AboutPage() {
+  return (
+    <div className="py-12">
+      <AboutSection />
+    </div>
+  );
+}
