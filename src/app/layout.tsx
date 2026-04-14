7408114e33c0bb24b4c78caeba6d0a10fd6f7ae4
abbaswork <abbaskhanwork@gmail.com>
2026-04-14 11:22:38 +0100

    feat: add gtm script on every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,11 +34,32 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PFQ4KMCD');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="preconnect" href="http://ec2-18-213-34-154.compute-1.amazonaws.com" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5979443933088571"
           crossOrigin="anonymous"></script>
       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} ${rajdhani.variable} antialiased font-sans bg-background text-foreground`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PFQ4KMCD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         {isEnabled && <PreviewNotice />}
         {children}
